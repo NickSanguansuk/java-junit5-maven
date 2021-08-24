8fbf04bca31015aeb6660d9908e0fcfbff249808
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-08-24 12:50:00 -0500

    Added Die class for test

--- src/main/java/die_demo/Die.java
+++ src/main/java/die_demo/Die.java
@@ -1,8 +1,31 @@
 package die_demo;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Die {
     // Data
     private Integer numberOffFaces;
     private Integer currentFaceValue;
+
+    // Constructors
+    public Die(Integer numberOffFaces) {
+        this.numberOffFaces = numberOffFaces;
+    }
+
+    // Getters and Setters
+    public Integer getNumberOffFaces() {
+        return numberOffFaces;
+    }
+
+    public Integer getCurrentFaceValue() {
+        return currentFaceValue;
+    }
+
+    // Methods
+    public void roll() {
+        ThreadLocalRandom randomNumberGenerator = ThreadLocalRandom.current();
+        Integer randomFaceValue = randomNumberGenerator.nextInt(1, numberOffFaces);
+        this.currentFaceValue = randomFaceValue;
+    }
 
 }
